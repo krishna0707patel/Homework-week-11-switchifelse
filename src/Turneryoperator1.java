ddad1c04162a5f0ba80afb810eabcb221ed31b1e
krishna0707patel <krishnapatel.0711@yahoo.com>
2022-10-12 16:36:49 +0100

    turnery

--- src/Turneryoperator1.java
+++ src/Turneryoperator1.java
@@ -1,11 +1,12 @@
 import java.util.Scanner;
-public class Turneryoperator {
+public class Turneryoperator1 {
     public static void main(String[] args) {
         System.out.println("Enter your number:  ");
         Scanner scanner = new Scanner(System.in); // calling new scanner class in main method
         int number = scanner.nextInt();
         String check = (number % 2 == 0) ? "Even" : "Odd";//turnery operator ?:
-        System.out.println("This given number\t" + number + "\tis: " + check);// using \t is giving a space between 2 values.
+        System.out.println("This given number\t" + number + "\tis: " + check);
+        // using \t is giving a space between 2 values.
     }
 
 }
